5e03efdae469c721c293784f10e9d901774d8be9
Lucas Matos <matoslucas@hotmail.com>
2019-01-14 11:06:38 -0700

    ignore firebase config file

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const Config = {
+    apiKey: "AIzaSyA7oRD08VAYxE1xc6tC7oFEn6Famj52TKU",
+    authDomain: "party-rock-tonight.firebaseapp.com",
+    databaseURL: "https://party-rock-tonight.firebaseio.com",
+    projectId: "party-rock-tonight",
+    storageBucket: "party-rock-tonight.appspot.com",
+    messagingSenderId: "414674366233"
+}
+export default Config
